b4e428bce21177885d471278221cd87668376744
千剑 <969308461@qq.com>
2019-04-25 16:31:23 +0800

    demo里加了Module2测试

---
+++ SimpleModuleRouterDemo/Module2/src/main/java/zm/myp/smr/module2/SmrModuleImpl.java
@@ -0,0 +1,17 @@
+package zm.myp.smr.module2;
+
+import zm.myp.smr.source.SmrApplication;
+import zm.myp.smr.source.SmrModule;
+
+/**
+ * Created by qianjian on 2019/4/25.
+ */
+
+public class SmrModuleImpl implements SmrModule {
+
+
+    @Override
+    public void register(SmrApplication app) {
+
+    }
+}
